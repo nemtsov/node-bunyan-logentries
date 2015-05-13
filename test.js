28f688546edb50b000cddf6d3804ca30cb43d635
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2015-05-13 18:41:17 +0300

    adds more tests

--- test.js
+++ test.js
@@ -16,13 +16,10 @@ describe('LogEntries Stream', function () {
 
     beforeEach(function () {
         logger = {
-            log: function () {
-            }
+            log: sinon.spy(),
+            end: sinon.spy()
         };
         stream = logentriesBunyan.createStream({token: 'atoken'});
-        sinon.stub(logger, 'log', function () {
-            //console.log('log:', arguments);
-        });
         stream._logger = logger;
     });
 
@@ -32,7 +29,35 @@ describe('LogEntries Stream', function () {
         expect(logger.log.args[0].length).to.equal(2);
     });
 
-    describe('log levels', function(){
+    describe('edge cases', function(){
+        it('throws when a token is not passed', function () {
+            expect(logentriesBunyan.createStream).to.throw('config.token must be set');
+        });
+
+        it('throws if nothing is passed', function(){
+            expect(stream.write).to.throw("nothing passed to log");
+        });
+
+        it('throws if the stream is closed', function () {
+            stream.end();
+            expect(stream.write.bind(stream, {level:10, msg:'I should throw'})).to.throw('failed to write to a closed stream');
+        });
+
+        it('logs the last message before the stream is closed', function(){
+            stream.end({level:10, msg: 'last message'});
+            expect(logger.log).to.have.been.called.once;
+            expect(logger.end).to.have.been.called.once;
+            expect(logger.log.args[0].length).to.equal(2);
+            expect(stream.write.bind(stream, {level:10, msg:'I should throw'})).to.throw('failed to write to a closed stream');
+        });
+
+        it('throws if the stream is destroyed', function () {
+            stream.destroy();
+            expect(stream.write.bind(stream, {level:10, msg:'I should throw'})).to.throw('failed to write to a closed stream');
+        });
+    });
+
+    describe('log levels', function () {
         it('translates bunyan level codes to strings', function () {
             stream.write({level: 10});
             expect(logger.log.args[0][0]).to.equal('trace');
@@ -54,32 +79,36 @@ describe('LogEntries Stream', function () {
         });
     });
 
-    describe('transform function', function(){
-        it('performs the given transform function', function(){
-            stream = logentriesBunyan.createStream({token: 'atoken'}, {transform: function(rec){
-                rec.message = rec.msg.toUpperCase();
-                delete rec.msg;
-                return rec;
-            }});
+    describe('transform function', function () {
+        it('performs the given transform function', function () {
+            stream = logentriesBunyan.createStream({token: 'atoken'}, {
+                transform: function (rec) {
+                    rec.message = rec.msg.toUpperCase();
+                    delete rec.msg;
+                    return rec;
+                }
+            });
             stream._logger = logger;
-            stream.write({level:20, msg: 'my message'});
+            stream.write({level: 20, msg: 'my message'});
             expect(logger.log.args[0][1].msg).to.not.exist;
             expect(logger.log.args[0][1].message).to.exist;
             expect(logger.log.args[0][1].message).to.equal('MY MESSAGE');
         });
 
-        it('if not transform function is provided it will log json', function(){
-            stream.write({level:20, msg: 'my message'});
+        it('if not transform function is provided it will log json', function () {
+            stream.write({level: 20, msg: 'my message'});
             expect(logger.log.args[0][1].msg).to.exist;
             expect(logger.log.args[0][1].msg).to.equal('my message');
         });
 
-        it('can log plain text using a transform function', function(){
-            stream = logentriesBunyan.createStream({token: 'atoken'}, {transform: function(rec){
-                return rec.level + '|'+ rec.hostname +'|' + rec.msg;
-            }});
+        it('can log plain text using a transform function', function () {
+            stream = logentriesBunyan.createStream({token: 'atoken'}, {
+                transform: function (rec) {
+                    return rec.level + '|' + rec.hostname + '|' + rec.msg;
+                }
+            });
             stream._logger = logger;
-            stream.write({level: 'debug', msg:'my message', hostname: 'my host'});
+            stream.write({level: 'debug', msg: 'my message', hostname: 'my host'});
             expect(logger.log.args[0][1]).to.equal('debug|my host|my message');
         });
     });
