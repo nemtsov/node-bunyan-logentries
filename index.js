98fd1f18aad86886a2e64d555a735f379e4d661e
Boite Witte <boite@vigour.io>
2016-03-22 10:55:19 +0100

    modified logentries default levels to bunyan default levels - fixes nemtsov/node-bunyan-logentries#6

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-'use strict';
+'use strict'
 
-var logentries = require('le_node'),
-  Stream = require('stream').Stream,
-  util = require('util'),
-  LBS;
+var Logentries = require('le_node')
+var Stream = require('stream').Stream
+var util = require('util')
+var LBS
 
-exports.LogentriesBunyanStream = LogentriesBunyanStream;
-exports.createStream = function (config, options) {
-  return new LogentriesBunyanStream(config, options);
-};
+module.exports = {
+  LogentriesBunyanStream: LogentriesBunyanStream,
+  createStream: function createStream (config, options) {
+    return new LogentriesBunyanStream(config, options)
+  }
+}
 
 /**
  * LogentriesBunyanStream
@@ -19,37 +21,40 @@ exports.createStream = function (config, options) {
  * @param {String} [config.defaultLevel] (defaults to `info`)
  */
 
-function LogentriesBunyanStream(config, options) {
-  if (!config || !config.token) throw new Error('config.token must be set');
-  Stream.call(this);
+function LogentriesBunyanStream (config, options) {
+  if (!config || !config.token) throw new Error('config.token must be set')
+  Stream.call(this)
 
-  this.transform = options && options.transform;
-  this.defaultLevel = options && options.defaultLevel || 'info';
-  this.writable = true;
+  this.transform = options && options.transform
+  this.defaultLevel = options && options.defaultLevel || 'info'
+  this.writable = true
 
-  config.levels = config.levels || ['debug', 'info', 'notice', 'warning', 'err', 'crit', 'alert', 'emerg'];
-  this._logger = new logentries(config);
+  // Default levels should be Buynan levels instead of default logentries levels:
+  // ['debug', 'info', 'notice', 'warning', 'err', 'crit', 'alert', 'emerg']
+  // See: https://github.com/trentm/node-bunyan#levels
+  config.levels = config.levels || ['trace', 'debug', 'info', 'warn', 'error', 'fatal']
+  this._logger = new Logentries(config)
 }
 
-util.inherits(LogentriesBunyanStream, Stream);
-LBS = LogentriesBunyanStream.prototype;
+util.inherits(LogentriesBunyanStream, Stream)
+LBS = LogentriesBunyanStream.prototype
 
 LBS.write = function (rec) {
-  if (!rec) throw new Error('nothing passed to log');
-  if (!this.writable) throw new Error('failed to write to a closed stream');
-  if ('function' === typeof this.transform) rec = this.transform(rec);
-  this._logger.log(this._resolveLevel(rec.level), rec);
-};
+  if (!rec) throw new Error('nothing passed to log')
+  if (!this.writable) throw new Error('failed to write to a closed stream')
+  if (typeof this.transform === 'function') rec = this.transform(rec)
+  this._logger.log(this._resolveLevel(rec.level), rec)
+}
 
 LBS.end = function (rec) {
-  if (arguments.length) this.write(rec);
-  this.writable = false;
-  this._logger.end();
-};
+  if (arguments.length) this.write(rec)
+  this.writable = false
+  this._logger.end()
+}
 
 LBS.destroy = function () {
-  this.writable = false;
-};
+  this.writable = false
+}
 
 LBS._resolveLevel = function (bunyanLevel) {
   var levelToName = {
@@ -59,6 +64,6 @@ LBS._resolveLevel = function (bunyanLevel) {
     40: 'warn',
     50: 'error',
     60: 'fatal'
-  };
-  return levelToName[bunyanLevel] || this.defaultLevel;
-};
+  }
+  return levelToName[bunyanLevel] || this.defaultLevel
+}
